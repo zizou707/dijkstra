83d77ba6efb287485cfcbf4d5626b5cbda46f2cf
zizou707 <162337899+zizou707@users.noreply.github.com>
2024-04-27 11:18:47 +0100

    Update Dijkstra.js

--- Dijkstra.js
+++ Dijkstra.js
@@ -14,30 +14,30 @@ class Graph {
         this.adjacencyList[vertex2][vertex1] = weight;
     }
 
-    
+    // creating function dijkstra
 
     dijkstra(source) {
-      let distances = {},
-      parents = {},
-      visited = new Set();
+      let distances = {},                // creating variables to update
+      parents = {},                      // distances between our source and other Nodes
+      visited = new Set();               //  creating new set that update visited nodes and parents object to update the neightbors Nodes 
 
-      for (let i = 0; i < this.vertices.length; i++) {
-        if (this.vertices[i] === source) {
-            distances[source] = 0;
+      for (let i = 0; i < this.vertices.length; i++) {     // looping in vertices Array that contaign all Nodes
+        if (this.vertices[i] === source) {                 // if its the source Node 
+            distances[source] = 0;                         // we update distance to this source to 0
         } else {
-            distances[this.vertices[i]] = Infinity;
+            distances[this.vertices[i]] = Infinity;        // or we set distances to infinty   
         }
-        parents[this.vertices[i]] = null;
+        parents[this.vertices[i]] = null;        // setting the source Node
     }
-    let currVertex = this.Mindist_N(distances, visited);
-
-        while (currVertex !== null) {
-            let distance = distances[currVertex],
-                neighbors = this.adjacencyList[currVertex];
-            for (let neighbor in neighbors) {
+    let currVertex = this.Mindist_N(distances, visited);    // now we create another variable to contaigh the current Node
+                                                            // after we calculate the min ditance by the function Mindist_N
+        while (currVertex !== null) {                           // while this node is not the current Node that we visiting
+            let distance = distances[currVertex],            // creating a variable to contaign distance to the current Node
+                neighbors = this.adjacencyList[currVertex];  // object that contaign the current neighbors Nodes and distances
+            for (let neighbor in neighbors) {                    // looping into neighbors Nodes
                 let newDistance = distance + neighbors[neighbor];
                 if (distances[neighbor] > newDistance) {
-                    distances[neighbor] = newDistance;
+                    distances[neighbor] = newDistance;             // updating distances when we find the shortest
                     parents[neighbor] = currVertex;
                 }
             }
@@ -48,12 +48,12 @@ class Graph {
         console.log(parents);
         console.log(distances);
     }
-    Mindist_N(distances, visited) {
-        let minDistance = Infinity,
+    Mindist_N(distances, visited) {               //  creating a function to calculate the short distance to
+        let minDistance = Infinity,               // next neightbor
             minVertex = null;
         for (let vertex in distances) {
             let distance = distances[vertex];
-            if (distance < minDistance && !visited.has(vertex)) {
+            if (distance < minDistance && !visited.has(vertex)) {  // here we test the min ditance and that node is not visited
                 minDistance = distance;
                 minVertex = vertex;
             }
